6064f300cc5f9198921f9c498d4b4b1ff2080a76
Beyoung Su Kang <81085595+BPT0@users.noreply.github.com>
2023-10-17 23:31:57 +0900

    fix. editeimgsactivity
    1.이전버튼 구현중
    2.tag 추가해 수정중

--- app/src/main/java/com/graduation/travelbook2/search/modify/EditImgActivity.kt
+++ app/src/main/java/com/graduation/travelbook2/search/modify/EditImgActivity.kt
@@ -2,6 +2,7 @@ package com.graduation.travelbook2.search.modify
 
 import android.os.Bundle
 import android.util.Log
+import androidx.fragment.app.Fragment
 import com.graduation.travelbook2.MyApplication
 import com.graduation.travelbook2.R
 import com.graduation.travelbook2.base.BaseActivity
@@ -17,9 +18,11 @@ class EditImgActivity : BaseActivity<ActivityEditImgBinding>() {
 
     private lateinit var selectedImgs: ArrayList<SelectedImgDto>
     private val listAddInfoImgFragment : ArrayList<AddInfoFragment> = ArrayList() // 프레그먼트 배열
+    private var currentFragment : Fragment? = null
+    private val fragmentTags : ArrayList<String> = ArrayList()
 
     private lateinit var imgSelectAdapter: ImgSelectedAdapter // 어답터
-
+    private var currentFragmentIndex = -1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +32,8 @@ class EditImgActivity : BaseActivity<ActivityEditImgBinding>() {
         createImgFragments()
         setFirstFragment()
         setSelectedImgRV()
+        setNextBtn()
+        setPrevBtn()
 
         binding.apply {
 
@@ -36,22 +41,52 @@ class EditImgActivity : BaseActivity<ActivityEditImgBinding>() {
 
     }
 
+    private fun setPrevBtn() {
+        // 현재 보여지는 프레그먼트 정보 얻기 -> 인덱스 정보 얻기
+        // 보여지는 프레그먼트를 이전 i -> i - 1 로 교체
+        //  if. 1번 인덱스라면 마지막 인덱스로 교체
+        binding.ibtnPrev.setOnClickListener {
+            currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_img_add_info)
+            supportFragmentManager.fragments.forEachIndexed { index, fragment ->
+                if(fragmentTags.contains(fragment.tag)){
+                    currentFragmentIndex = index
+                    return@forEachIndexed
+                }
+            }
+            if (currentFragmentIndex != -1) {
+                // 현재 표시된 프레임의 인덱스 출력
+                Log.d("Current Fragment", "Index: $currentFragmentIndex")
+            } else {
+                Log.d("Current Fragment", "No matching fragment found")
+            }
+        }
+
+    }
+
+    private fun setNextBtn() {
+        // 프레그먼트를 다음 i -> i+1 로 교체
+        //  if. i+1 이 마지막 인덱스라면 1번 인덱스로 교체
+    }
+
     private fun createImgFragments() {
         for (i: Int in 0 until selectedImgs.size){
+            fragmentTags.add("img$i")
             // 프레그먼트에 필요한 리스트의 정보 삽입
             listAddInfoImgFragment.add(AddInfoFragment.newInstance())
             val bundle= Bundle()
             bundle.putSerializable("imgInfo", selectedImgs[i].imgInfo)
             listAddInfoImgFragment[i].arguments = bundle
+
         }
     }
 
     private fun setFirstFragment() {
         supportFragmentManager.beginTransaction().replace(
-            R.id.fragment_img_add_info, listAddInfoImgFragment[0]).commit()
+            R.id.fragment_img_add_info, listAddInfoImgFragment[0], "img0").commit()
+
         for (i: Int in 1 until listAddInfoImgFragment.size){
             supportFragmentManager.beginTransaction().add(
-                R.id.fragment_img_add_info, listAddInfoImgFragment[i]
+                R.id.fragment_img_add_info, listAddInfoImgFragment[i], "img$i"
             ).commit()
         }
     }
@@ -86,6 +121,10 @@ class EditImgActivity : BaseActivity<ActivityEditImgBinding>() {
                                 .commit()
                         }
                     }
+
+                    currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_img_add_info)
+
+
                 }
 
             })
